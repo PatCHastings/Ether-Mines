fe0641b35aa7248cbe76ebd3e9bca618a19a8a28
patCHastings <phastings82@gmail.com>
2024-05-09 15:16:51 -0400

    imported forest scene, reference character, basic tileset for mineable environment. added components for collision and physics

---
+++ Assets/charScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.name = "Camera";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
